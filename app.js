faef51e2823be68456b536ba6720bed39e6f7c71
avcoder <code.rom7.6@gmail.com>
2021-10-08 23:15:32 -0400

    moved url from restcountries.eu to restcountries.com v3

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-import leafletMap from './showmap.js';
-import { getRandomNum, formatNumber } from './utils.js';
+import leafletMap from "./showmap.js";
+import { getRandomNum, formatNumber } from "./utils.js";
 
 // eventually need to update these nodes
-const btn = document.querySelector('button');
-const answer = document.querySelector('#answer');
-const img = document.querySelector('img');
+const btn = document.querySelector("button");
+const answer = document.querySelector("#answer");
+const img = document.querySelector("img");
 
-const capitalText = document.querySelector('#capitalText');
-const populationText = document.querySelector('#populationText');
-const currencyText = document.querySelector('#currencyText');
-const languagesText = document.querySelector('#languagesText');
-const soundAnswer = document.querySelector('audio');
+const capitalText = document.querySelector("#capitalText");
+const populationText = document.querySelector("#populationText");
+const currencyText = document.querySelector("#currencyText");
+const languagesText = document.querySelector("#languagesText");
+const soundAnswer = document.querySelector("audio");
 soundAnswer.volume = 0.07;
 
 let timeoutId = 0;
-
 
 // we are going to store our fetched json countries here
 const countries = [];
@@ -23,78 +22,97 @@ const countries = [];
 let country = {};
 
 // get all the countries and their data in JSON format
-fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then(data => {
-        // we want to take that data and store it locally
-        countries.push(...data.filter(country => country.latlng.length === 2));
-        leafletMap.mapInit('mapid');
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
+    // we want to take that data and store it locally
+    countries.push(...data.filter((country) => country.latlng.length === 2));
+    leafletMap.mapInit("mapid");
 
-        startGame();
-    });
+    startGame();
+  });
 
+function getLang(languages) {
+  const langs = Object.entries(languages).map((lang) => lang[1]);
+  return langs;
+}
+
+function getCurr(currencies) {
+  const currs = Object.entries(currencies).map((cur) => cur[1].name);
+  return currs;
+}
 
 function getRandomCountry() {
-    const index = getRandomNum(countries.length);
-    const { name, flag, latlng, capital, population, currencies, languages } = countries[index];
+  const index = getRandomNum(countries.length);
+  const {
+    name: { common: name },
+    capital: [capital, ...rest],
+    flags: { svg: flag },
+    latlng,
+    population,
+  } = countries[index];
 
-    country = { name, flag, latlng, capital, population, currencies, languages };
+  const languages = getLang(countries[index].languages);
+  const currencies = getCurr(countries[index].currencies);
+  country = { name, capital, flag, latlng, population, languages, currencies };
 
-    // remove that country from our array
-    countries.splice(index, 1); 
-    console.log(country)
-    return country;
+  // remove that country from our array
+  countries.splice(index, 1);
+  return country;
 }
 
 function startGame() {
-    if (countries.length <=1 ) {
-        btn.textContent = 'No more countries';
-        btn.removeEventListener('click', btnClicked);
-        return;
-    }
-    country = { ...getRandomCountry() };
-    img.src = country.flag;
+  if (countries.length <= 1) {
+    btn.textContent = "No more countries";
+    btn.removeEventListener("click", btnClicked);
+    return;
+  }
+  country = { ...getRandomCountry() };
+  img.src = country.flag;
 }
 
 function showAnswer() {
-    answer.textContent = country.name;
-    btn.textContent = "Play Again";
-    capitalText.textContent = country.capital;
-    populationText.textContent = formatNumber(country.population);
-    currencyText.textContent = country.currencies.map(money => `${money.name}`).join(', ');
-    languagesText.textContent = country.languages.map(lang => lang.name).join(', ');
-    
-    // add button animation 
-    timeoutId = setTimeout(() => {
-        btn.classList.add('pulse');
-    }, 3000)
+  answer.textContent = country.name;
+  btn.textContent = "Play Again";
+  capitalText.textContent = country.capital ?? "n/a";
+  populationText.textContent = formatNumber(country.population);
+  currencyText.textContent = country.currencies.join(", ");
+  languagesText.textContent = country.languages.join(", ");
 
-    // rewind then play sound
-    soundAnswer.currentTime = 0;
-    soundAnswer.play();
+  // add button animation
+  timeoutId = setTimeout(() => {
+    btn.classList.add("pulse");
+  }, 3000);
+
+  // rewind then play sound
+  soundAnswer.currentTime = 0;
+  soundAnswer.play();
 }
 
 function clearAnswer() {
-    leafletMap.remove();
-    answer.textContent = '';
-    capitalText.textContent = '';
-    populationText.textContent = '';
-    currencyText.textContent = '';
-    languagesText.textContent = '';
+  leafletMap.remove();
+  answer.textContent = "";
+  capitalText.textContent = "";
+  populationText.textContent = "";
+  currencyText.textContent = "";
+  languagesText.textContent = "";
 }
 
 function btnClicked() {
-    if (btn.textContent == "Answer") {
-        btn.classList.remove('pulse');
-        leafletMap.show({lat: country.latlng[0], lng: country.latlng[1], name: country.name });
-        showAnswer();
-    } else {
-        clearTimeout(timeoutId);
-        btn.textContent = "Answer";
-        clearAnswer();
-        startGame();
-    }
+  if (btn.textContent == "Answer") {
+    btn.classList.remove("pulse");
+    leafletMap.show({
+      lat: country.latlng[0],
+      lng: country.latlng[1],
+      name: country.name,
+    });
+    showAnswer();
+  } else {
+    clearTimeout(timeoutId);
+    btn.textContent = "Answer";
+    clearAnswer();
+    startGame();
+  }
 }
 
-
-btn.addEventListener('click', btnClicked);
+btn.addEventListener("click", btnClicked);
--- showmap.js
+++ showmap.js
@@ -2,29 +2,29 @@ var layer;
 var map;
 
 function mapInit(nodeId) {
-   map = L.map(nodeId);
+  map = L.map(nodeId);
 
-   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
 }
 
 function show({ lat, lng, name }) {
-    map.setView([lat, lng], 3);
-    layer = L.marker([lat, lng]).addTo(map);
-    layer.bindPopup(name, {className: 'tooltip'});
-    layer.openPopup();
+  map.setView([lat, lng], 3);
+  layer = L.marker([lat, lng]).addTo(map);
+  layer.bindPopup(name, { className: "tooltip" });
+  //   layer.openPopup();
 }
 
 function remove() {
-    layer.remove();
+  layer.remove();
 }
 
 const leafletMap = {
-    mapInit,
-    show,
-    remove
+  mapInit,
+  show,
+  remove,
 };
 
 export default leafletMap;
